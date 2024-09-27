186c1725248b73ae45ffcea953460e039fef61cb
Akshaya <akshayapalchamy@gmail.com>
2024-09-27 17:10:54 +0530

    ADDED: Folder Structure

---
+++ StudentManagementSystem.Web/Students/AddStudent.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddStudent.aspx.cs" Inherits="StudentManagementSystem.Web.Students.AddStudent" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>StudentManagementSystem</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="StudentName" runat="server">Name</asp:Label>
+            <asp:TextBox ID="StudentNameINput" runat="server"></asp:TextBox><br /><br />
+
+            <asp:Label ID="StudentID" runat="server">StudentID</asp:Label>
+            <asp:TextBox ID="StudentIDInput" runat="server"></asp:TextBox><br /><br />
+
+            <asp:Label ID="StudentClass" runat="server">Class</asp:Label>
+            <asp:TextBox ID="StudentClassInput" runat="server"></asp:TextBox><br /><br />
+
+            <asp:Label ID="StudentAddress" runat="server">Address</asp:Label>
+            <asp:TextBox ID="StudentAddressInput" runat="server"></asp:TextBox><br /><br />
+
+            <asp:Button ID="SaveButton" runat="server" Text="Save" OnClick="AddStudent_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ StudentManagementSystem.Web/Students/AddStudent.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+namespace StudentManagementSystem.Web.Students
+{
+    public partial class AddStudent : System.Web.UI.Page
+    {
+        public string ConnectionString = "Server=MS-NB0006\\MSSQLSERVER15; Database=ADO; User Id=sa; Password=password-123;";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CreateTableIfNotExists();
+            }
+        }
+
+        protected void CreateTableIfNotExists()
+        {
+            string strInitializeTable = @"IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Students' AND xtype='U')
+                                            Create Table Students(
+                                            Name VARCHAR(100),
+                                            StudentID int,
+                                            Class VARCHAR(10),
+                                            AddressName VARCHAR(100))";
+            using (SqlConnection Conn = new SqlConnection(ConnectionString))
+            {
+                SqlCommand Cmd = new SqlCommand(strInitializeTable, Conn);
+                Conn.Open();
+                Cmd.ExecuteNonQuery();
+            }
+        }
+
+        protected void AddStudent_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection Conn = new SqlConnection(ConnectionString))
+            {
+                string strInsertQuery = @"INSERT INTO Students(Name, StudentID, Class, AddressName)
+                                          VALUES(@Name, @StudentID, @Class, @AddressName)";
+
+                using (SqlCommand Command = new SqlCommand(strInsertQuery, Conn))
+                {
+
+                    Command.Parameters.AddWithValue("@Name", StudentNameINput.Text);
+                    Command.Parameters.AddWithValue("@StudentID", StudentIDInput.Text);
+                    Command.Parameters.AddWithValue("@Class", StudentClassInput.Text);
+                    Command.Parameters.AddWithValue("@AddressName", StudentAddressInput.Text);
+
+                    Conn.Open();
+                    Command.ExecuteNonQuery();
+                }
+
+            }
+
+
+
+        }
+    }
+}
